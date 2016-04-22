f06a07aaa27fee664d8a7d523d98b5c8f7050c9a
Heidi <heidi@apartmentlist.com>
2016-04-22 15:49:11 +1200

    auth working, almost at sessions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var request = require('superagent')
 var knexConfig = require('./knexfile')
 var knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"])
+var bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -30,25 +31,41 @@ function addTweet (tweet, username) {
 }
 
 
-app.get('/', function(req, res) { // grab data from database and render onto page
+app.get('/home', function(req, res) { // grab data from database and render onto page
   list()
   .then(function(data){
-   res.render("index", {tweet: data})
+   res.render("home", {tweet: data})
  })
 });
 
-app.post('/', function(req, res){
+app.post('/home', function(req, res){
   addTweet(req.body.tweet, req.body.username)
   .then(function(data){
-    res.redirect('/') //'once you've put the data into the db, go back to homepage'
+    res.redirect('/home') //'once you've put the data into the db, go back to homepage'
   })
 })
 
+app.get('/', function (req, res) {
+  res.render('sign-up')
+})
 
 
-// app.post('tweets', function(req,res) {
+app.post('/', function (req, res) {
+  var hash = bcrypt.hashSync(req.body.password, 10);
+  console.log("name", req.body.name)
+  knex('tweet').insert({name: req.body.name, password: hash}) // Store hash in your password DB.
+   .then(function(data){
+    // req.session.id = data //try person id
+    res.redirect('/home')
+    console.log("successful", data)
+    })
+  .catch(function(error){
+    console.log('Error', error)
+    req.session.userId = 0
+    res.redirect('/')
+  })
+});
 
-// })
 
 
 
