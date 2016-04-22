3bb3a7a40fd06911e8ec911c39f66deb24e554c5
Heidi <heidi@apartmentlist.com>
2016-04-22 14:46:30 +1200

    post and get working

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('bodyParser'); // check this, I think you only need it if your passing in Json?
+var bodyParser = require('body-parser'); // check this, I think you only need it if your passing in Json?
 var fs = require('fs');
 var request = require('superagent')
+var knexConfig = require('./knexfile')
+var knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"])
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'hbs');
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/views/index.html'); // send file to client
+//add server
+app.listen(process.env.PORT || 3000, function () {
+  console.log('listening on port 3000!');
 });
 
-app.get('/results', function(req, res){
-  res.sendFile(__dirname + '/views/index.html'); // send file to client
+function list(){
+  return knex.raw('SELECT * FROM "tweet"' )
+}
+
+function addTweet (tweet, username) {
+  // return knex.raw('insert into "tweet" (tweet, name) values ("' + tweet + '", "'+username+'" );')
+  return knex.raw('insert into "tweet" (tweet, name) values (?,?);', [tweet, username]) //'I'm saving you from sneaky sql injection attacks!'
+}
+
+
+app.get('/', function(req, res) { // grab data from database and render onto page
+  list()
+  .then(function(data){
+   res.render("index", {tweet: data})
+ })
 });
 
-app.get('/', function(req, res) {
-
+app.post('/', function(req, res){
+  addTweet(req.body.tweet, req.body.username)
+  .then(function(data){
+    res.redirect('/') //'once you've put the data into the db, go back to homepage'
+  })
 })
 
-app.post('tweets', function(req,res) {
-
-  }
 
 
+// app.post('tweets', function(req,res) {
 
-modules.export = app;
+// })
+
+
+
+// spelling :-)
+module.exports = app;
