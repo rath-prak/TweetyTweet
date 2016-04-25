2def83cfe6e459846a073f7783e019221d6e478c
lizzielashark <lizzie.s.sullivan@gmail.com>
2016-04-25 13:32:34 +1200

    refactored 1

---
+++ setup.js
@@ -0,0 +1,29 @@
+var bcrypt = require('bcrypt');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser'); // check this, I think you only need it if your passing in Json?
+var fs = require('fs');
+var request = require('superagent')
+var knexConfig = require('./knexfile')
+var knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"])
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'view')));
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'hbs');
+
+//add server
+app.listen(process.env.PORT || 3000, function () {
+  console.log('listening on port 3000!');
+});
+
+module.exports = {
+  app: app,
+  knex: knex,
+  bcrypt: bcrypt
+}
